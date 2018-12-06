d336cc4f562e0b23dbd30e964a8f8901a2b61d84
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-12-06 19:45:57 +0300

    add classes

---
+++ src/a/b/c/R9Class.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 12/6/2018.
+ * Project: multiple_repos
+ * *******************************
+ */
+public class R9Class {
+}
